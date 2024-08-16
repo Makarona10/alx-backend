3e9b336ba1d0aa6498b899a5d52ff46175aa50b5
Makarona10 <ahmedneyssi@gmail.com>
2024-08-16 06:17:08 +0300

    Task 2 done

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+'''0x00. Pagination'''
+
+import csv
+import math
+from typing import Tuple, Dict, Union, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    '''return a tuple of size two containing a start index and an end index'''
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''gets the pages of the intended data'''
+        assert type(page) is int and page > 0 and type(page_size) \
+            is int and page_size > 0
+        data_set = self.dataset()
+        range = index_range(page, page_size)
+        if (len(data_set) <= (page - 1) * page_size):
+            return []
+        return list(data_set[range[0]: range[1]])
+
+    def get_hyper(self, page: int = 1,
+                  page_size: int = 10) -> Dict[str, Union[int, str]]:
+
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        page_data = self.get_page(page, page_size)
+        nxt_page = page + 1 if total_pages > page else None
+        prev_page = None if page == 1 else page - 1
+        return {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': nxt_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
