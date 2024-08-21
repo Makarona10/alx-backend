fadbf4945105708c9860a0ac280d10766f692f5c
Makarona10 <ahmedneyssi@gmail.com>
2024-08-21 05:44:26 +0300

    Task 0 done

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,14 +6,17 @@ BaseCaching = __import__('BaseCacheClass').BaseCaching
 
 class BasicCache(BaseCaching):
     def __init__(self):
+        '''BasicCache initialization'''
         super.__init__()
 
     def put(self, key, item):
+        '''Add a new process to cache'''
         if not key or not item:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        '''Gets a process from the cache'''
         if not key or not self.cache_data.get(key):
             return
         return self.cache_data.get(key)
